19652b121121ad78674c25a82b192485f479fb8d
ACER <sirilalita33@gmail.com>
2019-10-13 13:35:40 +0700

    update

--- index.js
+++ index.js
@@ -246,7 +246,7 @@ function handleMessageEvent(event) {
         
         msg = {
             type: 'text',
-            text: 'น้องบอทสามารถตอบคำถามเกี่ยวกับ\ที่อยู่\เบอร์ติดต่อ\เบอร์ติดต่อการตลาด\อีเมล\เว็บไซต์\facebook\แฟกซ์\ติดต่อภายใน'
+            text: 'น้องบอทสามารถตอบคำถามเกี่ยวกับ\n-ที่อยู่\n-เบอร์ติดต่อ\n-เบอร์ติดต่อการตลาด\n-อีเมล\n-เว็บไซต์\n-facebook\n-แฟกซ์\n-ติดต่อภายใน'
         };
         if (eventText!== "hello, world" && eventText!== null) {
             db.all("INSERT INTO  question(question) VALUES(?)", [eventText], (err) => {
